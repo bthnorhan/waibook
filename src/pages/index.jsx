8f04f73005cebd7b88bb28ebce4fd3ffdd54da0b
Batuhan Orhan <bthnorhan00@gmail.com>
2023-02-19 00:59:29 +0300

    Refactor pages

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '@/styles/Home.module.css';
+
+import { Banner, NavBar } from '@/components';
 
 const Home = () => {
 	return (
 		<>
 			<Head>
 				<title>WaiBook</title>
-				<meta
-					name='description'
-					content='WaiBook book website template powered by Next.js '
-				/>
+				<meta name='description' content='WaiBook website template powered by Next.js ' />
 				<meta name='viewport' content='width=device-width, initial-scale=1' />
 				<link rel='icon' href='/favicon.ico' />
 			</Head>
-			<main></main>
+			<main>
+				<NavBar />
+				<Banner />
+			</main>
 		</>
 	);
 };
