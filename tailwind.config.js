baa0ae96ea81616390a1c988eea74a736296a024
Batuhan Orhan <bthnorhan00@gmail.com>
2023-02-19 00:59:10 +0300

    Edit tailwind config and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
 	content: ['./src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: '#6055A0',
+				'primary-dark': '#4c4380',
+			},
+		},
 	},
 	plugins: [],
 };
